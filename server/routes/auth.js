5229e6b71672c1ea5288093f8dcc59389d6b80a3
moose <mr.rammo@gmail.com>
2019-04-18 18:48:11 -0400

    removing unnecesary dependency

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../config.json');
-const secret_config = require('../config.secret.json');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('../models/User');
